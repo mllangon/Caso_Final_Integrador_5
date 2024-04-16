4253d7220410904b70dcce499e780d333b1c3e0d
mllangon <mllangon@myuax.com>
2024-04-16 18:38:01 +0200

    5

--- src/main/java/Analisis_Numerico/CalculoPotencias.java
+++ src/main/java/Analisis_Numerico/CalculoPotencias.java
@@ -1,4 +1,9 @@
 package Analisis_Numerico;
 
 public class CalculoPotencias {
+
+    public int calcularPotencia(int base, int exponente) {
+        if (exponente == 0) return 1;
+        return base * calcularPotencia(base, exponente - 1);
+    }
 }
