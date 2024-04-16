b46f34795f78647c90b1dca44c15e90f8aac2f78
mllangon <mllangon@myuax.com>
2024-04-16 18:37:48 +0200

    4

--- src/main/java/Analisis_Numerico/ListadoNumeros.java
+++ src/main/java/Analisis_Numerico/ListadoNumeros.java
@@ -1,4 +1,16 @@
 package Analisis_Numerico;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListadoNumeros {
+
+    public List<Integer> listarNumeros(int inicio, int fin) {
+        if (inicio > fin) {
+            return new ArrayList<>();
+        }
+        List<Integer> lista = listarNumeros(inicio, fin - 1);
+        lista.add(fin);
+        return lista;
+    }
 }
