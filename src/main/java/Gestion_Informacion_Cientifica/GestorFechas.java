80ebd4c30182120dbd0ed375c56a65144779e251
mllangon <mllangon@myuax.com>
2024-04-16 18:48:50 +0200

    9

--- src/main/java/Gestion_Informacion_Cientifica/GestorFechas.java
+++ src/main/java/Gestion_Informacion_Cientifica/GestorFechas.java
@@ -1,4 +1,27 @@
 package Gestion_Informacion_Cientifica;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class GestorFechas {
+
+    private List<Date> fechas = new ArrayList<>();
+
+    public void agregarFecha(String fecha) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = sdf.parse(fecha);
+        fechas.add(date);
+        Collections.sort(fechas);
+    }
+
+    public List<String> obtenerFechas() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        List<String> fechasFormato = new ArrayList<>();
+        for (Date fecha : fechas) {
+            fechasFormato.add(sdf.format(fecha));
+        }
+        return fechasFormato;
+    }
 }
