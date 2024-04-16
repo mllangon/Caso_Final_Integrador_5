b955c5e56f0cb6e2516c27fbb9b890eb13594d11
mllangon <mllangon@myuax.com>
2024-04-16 18:38:16 +0200

    6

--- src/main/java/Analisis_Numerico/MaximoValor.java
+++ src/main/java/Analisis_Numerico/MaximoValor.java
@@ -1,4 +1,18 @@
 package Analisis_Numerico;
 
 public class MaximoValor {
+
+    public int encontrarMaximo(int[] datos) {
+        return encontrarMaximoRecursivo(datos, 0, datos.length - 1);
+    }
+
+    private int encontrarMaximoRecursivo(int[] datos, int inicio, int fin) {
+        if (inicio == fin) {
+            return datos[inicio];
+        }
+        int medio = (inicio + fin) / 2;
+        int maxIzquierda = encontrarMaximoRecursivo(datos, inicio, medio);
+        int maxDerecha = encontrarMaximoRecursivo(datos, medio + 1, fin);
+        return Math.max(maxIzquierda, maxDerecha);
+    }
 }
