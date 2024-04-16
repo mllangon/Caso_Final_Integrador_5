68bd5126d6a8aeaef81445285b07e68b3137ca51
mllangon <mllangon@myuax.com>
2024-04-16 18:48:30 +0200

    9

--- src/main/java/Gestion_Informacion_Cientifica/BusquedaTexto.java
+++ src/main/java/Gestion_Informacion_Cientifica/BusquedaTexto.java
@@ -1,4 +1,32 @@
 package Gestion_Informacion_Cientifica;
 
+import java.util.Arrays;
+
 public class BusquedaTexto {
+
+    // Búsqueda lineal
+    public boolean buscarLineal(String[] texto, String palabra) {
+        for (String linea : texto) {
+            if (linea.contains(palabra)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Búsqueda binaria, asumiendo que el texto está ordenado
+    public boolean buscarBinaria(String[] texto, String palabra) {
+        int izquierda = 0, derecha = texto.length - 1;
+        while (izquierda <= derecha) {
+            int medio = izquierda + (derecha - izquierda) / 2;
+            if (texto[medio].compareTo(palabra) == 0) {
+                return true;
+            } else if (texto[medio].compareTo(palabra) < 0) {
+                izquierda = medio + 1;
+            } else {
+                derecha = medio - 1;
+            }
+        }
+        return false;
+    }
 }
