d04155249504ec69c5c182898a5f4de1804a3459
mllangon <mllangon@myuax.com>
2024-04-16 18:26:52 +0200

    1

--- src/main/java/Analisis_Genomico/Combinaciones.java
+++ src/main/java/Analisis_Genomico/Combinaciones.java
@@ -1,4 +1,18 @@
 package Analisis_Genomico;
 
 public class Combinaciones {
+
+    // Método recursivo para calcular combinaciones
+    public int calcularCombinaciones(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return calcularCombinaciones(n - 1) + calcularCombinaciones(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Combinaciones combinaciones = new Combinaciones();
+        int n = 5;
+        System.out.println(combinaciones.calcularCombinaciones(n));
+    }
 }
